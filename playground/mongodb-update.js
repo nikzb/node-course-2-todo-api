fc9c14365d4301193b626f89c32957a8daf8c966
Nik <nik@Niks-MacBook-Pro.local>
2017-01-24 20:36:20 -0500

    Add update examples

---
+++ playground/mongodb-update.js
@@ -0,0 +1,43 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+var obj = new ObjectID();
+console.log(obj);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // findOneAndUpdate
+
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('58877aa3f47017059bb8a3f6')
+  // }, {
+  //   $set: {  // using update operator
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('5887f37339008bf327afdaa2')
+  }, {
+    $set: {
+      name: 'Bobby'
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+  // db.close();
+});
