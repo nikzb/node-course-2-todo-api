c73b19e951708e94ef9cd70b9cf037b69a574e15
Nik <nik@Niks-MacBook-Pro.local>
2017-01-25 13:35:23 -0500

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
@@ -27,6 +28,32 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  // make sure id is valid
+  if (!ObjectID.isValid(id)) {
+    // if not valid, respond with 404 - send back empty send
+    return res.status(404).send();
+  }
+
+  // findById - look for matching document
+  Todo.findById(id).then((todo) => {
+    if (todo) {
+      res.send(todo);
+    } else {
+      res.status(404).send();
+    }
+  }).catch((e) => {
+    res.status(400).send();
+  });
+    // success
+      // if found, send it back
+      // if not, send back 404 with empty body
+    // Error
+      // 400 - and send empty body back
 });
 
 app.listen(3000, () => {
