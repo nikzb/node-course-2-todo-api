4ff53ea3c9e6e30b9dd21d311261ce14bfd63462
Nik <nik@Niks-MacBook-Pro.local>
2017-01-24 19:23:23 -0500

    Initial commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,36 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+var obj = new ObjectID();
+console.log(obj);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('58877aa3f47017059bb8a3f6')
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos');
+  // });
+
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log(`Todos count: ${count}`);
+  //
+  // }, (err) => {
+  //   console.log('Unable to fetch count');
+  // });
+
+  db.collection('Users').find({name:'Nik'}).count().then((count) => {
+    console.log(`There are ${count} Niks`);
+  }, (err) => {
+    console.log('Unable to complete the operation');
+  });
+
+  // db.close();
+});
