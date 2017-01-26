2416e6161c1a562b3fffa683d1d158abb3c1ab21
Nik <nik@Niks-MacBook-Pro.local>
2017-01-25 19:41:48 -0500

    Add test cases for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -42,7 +42,7 @@ app.get('/todos/:id', (req, res) => {
   // findById - look for matching document
   Todo.findById(id).then((todo) => {
     if (todo) {
-      res.send(todo);
+      res.send({todo});
     } else {
       res.status(404).send();
     }
