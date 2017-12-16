3e134994145860b8a9c3d543cdad1cb47ad373d8
Nik <NikBaltatzis@gmail.com>
2017-12-16 14:47:03 -0500

    Add GET /users/me and authentication middleware

---
+++ server/middleware/authenticate.js
@@ -0,0 +1,21 @@
+const {User} = require('./../models/user');
+
+const authenticate = async (req, res, next) => {
+  const token = req.header('x-auth');
+
+  try {
+    const user = await User.findByToken(token);
+
+    if (!user) {
+      return Promise.reject();
+    }
+
+    req.user = user;
+    req.token = token;
+    next();
+  } catch (e) {
+    res.status(401).send();
+  }
+};
+
+module.exports = {authenticate};
--- server/models/user.js
+++ server/models/user.js
@@ -50,6 +50,27 @@ UserSchema.methods.generateAuthToken = function() {
   });
 };
 
+UserSchema.statics.findByToken = function(token) {
+  const User = this;
+  let decoded;
+
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch (e) {
+    // return new Promise((resolve, reject) => {
+    //   reject();
+    // });
+    // shorter version of commented out code above
+    return Promise.reject();
+  }
+
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
+}
+
 const User = mongoose.model('User', UserSchema);
 
 module.exports = { User };
